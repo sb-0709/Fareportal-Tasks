a45096ae74605a61a7b6b3391f55900fbd56d2b0
Sanskriti Bansal <76274143+sb-0709@users.noreply.github.com>
2024-01-11 16:18:31 +0530

    Update EmpClient.cs

--- task1/EmpClient.cs
+++ task1/EmpClient.cs
@@ -4,22 +4,22 @@ namespace task1{
     class EmpClient{
         public static void Main(){
             //for employee class derived by permenant and trainee class
-            // Permenant e = new Permenant();
-            // Console.WriteLine("\n....Permenant Employee.....\n");
-            // e.AcceptDetails();
-            // e.GetDetails();
-            // e.CalculateSalary();
-            // Console.WriteLine("\n....Details of Permenant Employee.....\n");
-            // e.DisplayDetails();
-            // e.ShowDetails();
-            // Trainee e1 = new Trainee();
-            // Console.WriteLine("\n\n....Trainee Employee.....\n");
-            // e1.AcceptDetails();
-            // e1.GetTraineeDetails();
-            // e1.CalculateSalary();
-            // Console.WriteLine("\n....Details of Trainee Employee.....\n");
-            // e1.DisplayDetails();
-            // e1.ShowTraineeDetails();
+            Permenant e = new Permenant();
+            Console.WriteLine("\n....Permenant Employee.....\n");
+            e.AcceptDetails();
+            e.GetDetails();
+            e.CalculateSalary();
+            Console.WriteLine("\n....Details of Permenant Employee.....\n");
+            e.DisplayDetails();
+            e.ShowDetails();
+            Trainee e1 = new Trainee();
+            Console.WriteLine("\n\n....Trainee Employee.....\n");
+            e1.AcceptDetails();
+            e1.GetTraineeDetails();
+            e1.CalculateSalary();
+            Console.WriteLine("\n....Details of Trainee Employee.....\n");
+            e1.DisplayDetails();
+            e1.ShowTraineeDetails();
 
             //for IEmployee interface implemented by IPermanant and ITrainee class
             Console.WriteLine("\n....Permenant Employee.....\n");
